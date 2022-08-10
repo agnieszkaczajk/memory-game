e2fd3add7f4dae920ec3b727d2343c717b989961
agnieszkaczajk <agnieszkaczajk@gmail.com>
2022-08-10 12:35:56 +0200

    Add Words.txt to resources, read it to List

--- src/main/java/org/java/academy/Main.java
+++ src/main/java/org/java/academy/Main.java
@@ -1,7 +1,31 @@
 package org.java.academy;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    static List<String> listOfWords;
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        listOfWords = new ArrayList<>();
+        initialization();
+        System.out.println("End of execution!");
     }
+
+    public static void initialization() throws IOException {
+        readWordsFile();
+    }
+
+    private static void readWordsFile() throws IOException {
+        String path = "src/main/resources/Words.txt";
+        try(FileInputStream fileInputStream = new FileInputStream(path);
+            Scanner s = new Scanner(fileInputStream)) {
+            while (s.hasNext()) {
+                listOfWords.add(s.next());
+            }
+        }
+    }
+
 }
